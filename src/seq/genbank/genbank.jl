a80978385fbf9b8a300edacc9e050d3ecf95626f
KEVIN BONHAM <kevbonham@gmail.com>
2017-03-24 13:57:21 -0400

    Begin work on genbank parser

---
+++ src/seq/genbank/genbank.jl
@@ -0,0 +1,20 @@
+# Genbank File Format
+# =================
+
+module Genbank
+
+import Automa
+import Automa.RegExp: @re_str
+import Bio: Bio, isfilled
+import Bio.Exceptions: missingerror
+import BufferedStreams
+
+export description,
+       identifier
+
+include("record.jl")
+include("index.jl")
+include("reader.jl")
+include("writer.jl")
+
+end
